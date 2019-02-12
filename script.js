f0e3cb32db73a7cbddaba8bff265c3ba05e86504
Arsal Mehran <arsalmehran@gmail.com>
2019-02-11 21:07:32 -0800

    added tests for searchDatabase function

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const database = [
 	'catsLmao.com'
 ];
 
-const searchDatabase = (searchInput) => {
-	const matches = database.filter(website => {
+const searchDatabase = (searchInput, db) => {
+	const matches = db.filter(website => {
 		return website.includes(searchInput);
 	});
 	return matches.length > 3 ? matches.slice(0, 3) : matches;
 }
 
-console.log(searchDatabase('cats'));
+console.log(searchDatabase('cats', database));
+
+module.exports = searchDatabase;
--- script.test.js
+++ script.test.js
@@ -1,3 +1,31 @@
-it('this is a test', () => {
-	
+const searchDatabase = require('./script');
+
+databaseMock = [
+	'harry.com',
+	'snape.com',
+	'ron.com',
+	'hermione.com',
+	'dog.com',
+	'dogsLmao.com'
+]
+
+describe('searchDatabase', () => {
+	it('is a hello test', () => {
+		expect('hello').toBe('hello');
+	})
+
+	it('is searching database', () => {
+		expect(searchDatabase('test', databaseMock)).toEqual([]);
+		expect(searchDatabase('dog', databaseMock)).toEqual(['dog.com', 'dogsLmao.com']);
+	})
+
+	it('does not return more than 3 matches', () => {
+		expect(searchDatabase('.com', databaseMock).length).toEqual(3);
+	})
+
+	it('works with undefined and null input', () => {
+		expect(searchDatabase(undefined, databaseMock)).toEqual([]);
+		expect(searchDatabase(null, databaseMock)).toEqual([]);
+	})
 })
+
